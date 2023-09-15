57c3f9a29fae4036e6d8c334ae3a11777e2a8def
cstirneman <cstirneman@bellarmine.edu>
2023-09-15 13:45:58 -0400

    First Commit

--- src/RecDieRollerDriver.java
+++ src/RecDieRollerDriver.java
@@ -9,19 +9,20 @@ public class RecDieRollerDriver {
 
 	public static void main(String[] args) {
 		int totalRolls = 0;
-        int numberOfRolls = 1000;
+        int numberOfRolls = 1000;//variable that is used for the times to perform the roll method
         
-		RecDieRoller rec = new RecDieRoller();	
+		RecDieRoller rec = new RecDieRoller();//create new instance of RecDieRoller
 		//rec.roll();
 		
+		//for loop for rolling a die and calculates the total number of rolls it takes to get a 2
         for (int i = 0; i < numberOfRolls; i++) {
-            int rollsToGet2 = rec.roll();
+            int rollsToGet2 = rec.roll();//
             totalRolls += rollsToGet2;
         }
 
-        double meanRolls = (double) totalRolls / numberOfRolls;
+        double meanRolls = (double) totalRolls / numberOfRolls; //calculates the mean of rolls it took to get a 2
         System.out.println("Mean number of rolls to get a '2': " + meanRolls);
 		
-	}
+	}//end main
 
-}
+}//end class
